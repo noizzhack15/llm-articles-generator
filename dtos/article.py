33d47c090d1711844ee78bfb4be4c3335c45194c
unknown <you@example.com>
2025-12-05 11:02:29 +0200

    fix

--- dtos/article.py
+++ dtos/article.py
@@ -8,3 +8,6 @@ class Article(BaseModel):
     title: str = Field(description="The professional title of the article.")
     summary: str = Field(description="A brief, 1-2 sentence summary of the article content.")
     final_output: str = Field(description="The complete, polished body of the article.")
+    article_field: str = Field(description="the field of the article.")
+    article_subdomain: str = Field(description="the subdomain of the article.")
+    country: str = Field(description="The country of the article.")
--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def send_article_to_queue(article: Article):
     print("\n--- Tool Execution: send_article_to_queue ---")
     print(f"Article Title: {article.title}")
     print(f"Article Summary: {article.summary}")
-    print(f"Article Content (First 50 chars): {article.final_output[:50]}...")
+    print(f"Article Content: {article.final_output}")
     print("Sending article object to queue successful.")
     print("------------------------------------------\n")
 
@@ -51,7 +51,7 @@ async def send_article_to_queue(article: Article):
         message,
         routing_key="test"
     )
-    
+
     return "Article successfully sent to the queue."
 
 
@@ -68,19 +68,14 @@ async def run_simple_text_generator_agent():
         tools=[send_article_to_queue]
     )
 
+    with open(
+            "c:/code_projects/breaking-bed/llm-articles-generator/prompts/eng/system_prompt.txt", 'r',
+            encoding='utf-8') as file:
+        articles_generator_agent_prompt = file.read()
+
     articles_generator_agent = Agent(
         name="articles generator agent",
-        instructions="""
-you are a professional journalist working for the leading daily news paper in the state of Israel. you cover the field of sports. you only write articles in that subject. 
-Follow these steps carefully:
-1. write articles in English only.
-2. write articles using correct grammar.
-3. the article you write must be interesting and professional.
-4. Handoff for Sending: pass the generated article object to "articles queue sender agent". this agent will take care of sending the generated article to a destination queue.
-
-Crucial Rules:
-- You must hand off exactly ONE Article object (with fields: title, summary, final_output) to the "articles queue sender agent" 
-""",
+        instructions=articles_generator_agent_prompt,
         output_type=Article,
         model="gpt-4o-mini",
         handoffs=[articles_queue_sender_agent]
@@ -88,7 +83,7 @@ Crucial Rules:
 
     result = await Runner.run(
         articles_generator_agent,
-        "write a short article about soccer - no longer than 20 words"
+        "write a short news article about soccer - no longer than 50 words"
     )
 
 
